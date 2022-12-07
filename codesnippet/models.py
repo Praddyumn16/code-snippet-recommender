0e69741a005e7c0f45bc8e4b5ec26472edc351c3
Praddyumn16 <spraddyumn@gmail.com>
2022-12-07 15:17:01 +0530

    code push

---
+++ codesnippet/models.py
@@ -0,0 +1,66 @@
+from flask import jsonify, request, session, render_template
+from datetime import datetime
+from app import db
+import uuid
+
+class Snippet:
+
+  """ ----------------------------------
+    add new code snippet to database
+    -----------------------------------"""
+  def add(self):
+    temp = request.form.get("keywords")
+    temp = temp.replace(" ", "")
+    # temp = temp.split(',')
+    keywds = temp.lower()
+    # keywds = [item.lower() for item in temp]
+    timeStr = datetime.now()
+
+    code = request.form.get("codesnippet")
+    code = code.strip()
+    print("striped code:", code)
+
+    snippet = {
+        "_id":uuid.uuid4().hex,
+        "username":session["user"]["name"],
+        "codeSnippet":code,
+        "keywords": keywds,
+        "uploadedOn": timeStr.strftime("%d/%m/%Y %H:%M:%S")
+    }
+
+    if db.snippets.insert_one(snippet):
+      print("snippet added", snippet)
+      return jsonify(snippet), 200
+
+    print("snippet adding failed", snippet)
+    return jsonify({"error": "could'nt add codesnippet."}), 400
+
+  """ ----------------------------------
+    search code snippet from database
+    -----------------------------------"""
+  def search(self, query):
+    item_details = db.snippets.find()
+    session['query'] = query
+
+    q = query.split(' ')
+    print(q)
+
+    snippet = []
+    print("type:item_details:", type(item_details), "type:item:", type(item_details[0]))
+    for item in item_details:
+      for i in q:
+        if i.lower() in item['keywords'] and len(i) > 2:
+          snippet.append(item)
+          break
+      # if query in item['keywords']:
+      #   snippet.append(item)
+      # for keyword in item['keywords']:
+      #   if keyword.find(query) or query.find(keyword):
+      #     print(item['codeSnippet'])
+      #     snippet.append(item)
+      #     break
+      # snippet.append(item)
+    
+    print(query)
+    snippet.reverse()
+    return render_template('home.html', codesnippets=snippet , query_input=query)
---
+++ user/routes.py
@@ -0,0 +1,47 @@
+import flask
+from app import app, render_template
+from user.models import User
+from functools import wraps
+from flask import session, redirect
+
+def login_required(f):
+  @wraps(f)
+  def wrap(*args, **kwargs):
+    if 'logged_in' in session:
+      return f(*args, **kwargs)
+    else:
+      return redirect('/login')
+  
+  return wrap
+
+@app.route('/register', methods=['POST'])
+def signup():
+  print("signup called")
+  return User().signup()
+
+@app.route('/login', methods=['GET'])
+def login():
+  print("login called")
+  return render_template('login.html'), 200
+
+@app.route('/signup', methods=['GET'])
+def sigunp():
+  print("signup called")
+  return render_template('signup.html'), 200
+
+@app.route('/auth', methods=['POST'])
+def auth():
+  print("auth called")
+  # make authentication here
+  return User().login()
+
+
+@app.route('/dashboard', methods=['GET'])
+@login_required
+def dashboard():
+  return render_template('home.html')
+
+
+@app.route('/logout', methods=['GET'])
+def logout():
+  return User().signout()
